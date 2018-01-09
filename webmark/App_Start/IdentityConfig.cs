fa3532c95adabe0c95eddbfbd31881917d1a8f97
mihaiborbea <borbeamihai@hotmail.com>
2018-01-09 02:12:55 +0200

    WIP cleaning boilerplate code

--- webmark/App_Start/IdentityConfig.cs
+++ webmark/App_Start/IdentityConfig.cs
@@ -50,10 +50,8 @@ namespace webmark
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
                 RequireDigit = true,
                 RequireLowercase = true,
-                RequireUppercase = true,
             };
 
             // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
